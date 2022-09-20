221b1b7dfde7ab8ed78033949873e709afe5e087
neopragma <davenicolette@gmail.com>
2022-09-20 13:27:45 -0700

    Sample mountains code

--- assets/scripts/load_mountains_data.js
+++ assets/scripts/load_mountains_data.js
@@ -1,6 +1,12 @@
 "use strict"
 
 let mountainsArray = []
+let showMeButton = document.querySelector("#showMe")
+let mountainNamesDropDown = document.querySelector("#mountainNames")
+let selectedMountain = ""
+let infoTable = document.querySelector("#infoTable")
+let mountainInfo = document.querySelector("#mountainInfo")
+
 
 window.onload = function(){
 
@@ -8,6 +14,47 @@ window.onload = function(){
         mountainsArray = mountains.mountains;
     })
 
+    showMeButton.addEventListener("click", () => {
+        loadNames(mountainsArray)
+        mountainNamesDropDown.classList.remove("d-none")
+        infoTable.classList.add("d-none")
+    })
+          
+    mountainNamesDropDown.addEventListener("change", (event) => {
+        selectedMountain = mountainsArray.filter((mountain) => {
+            return mountain.name === event.target.value
+        })
+        event.target.value = "Choose a mountain"
+        mountainNamesDropDown.classList.add("d-none")
+        loadCardFor(selectedMountain[0])
+    })
+}
+
+function loadCardFor(mountain) {
+    var txt = '<div class="card">'
+    txt += '<div class="card-body">'
+    txt += '<p>'
+    txt += 'Name: '
+    txt += mountain.name 
+    txt += '</p>'
+    txt += '<p>'
+    txt += 'Elevation: '
+    txt += mountain.elevation 
+    txt += '</p>'
+    txt += '<p>'
+    txt += 'Effort: '
+    txt += mountain.effort 
+    txt += '</p>'
+    txt += '</div>'
+    mountainInfo.innerHTML = txt
+    infoTable.classList.remove("d-none")
+}
+
+
+function loadNames(mountainsArray) {
+    mountainsArray.forEach((mountain) => {
+        mountainNames.innerHTML += `<option value="${mountain.name}">${mountain.name}</option>`
+    })
 }
 
 //function that can "fetch" the sunset/sunrise times
