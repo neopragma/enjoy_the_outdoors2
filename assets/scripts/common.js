50d65eed5f97411096e4ea9d998a4a0da51bdf5a
neopragma <davenicolette@gmail.com>
2022-09-21 09:25:15 -0700

    Tweaks to sample code

--- assets/scripts/common.js
+++ assets/scripts/common.js
@@ -2,3 +2,8 @@
 function wrapAsOption(value) {
     return `<option value="${value}">${value}</option>`
 }
+
+function capitalize(value) {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`
+}
+
